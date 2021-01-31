a9ce480a41300ff8c785566216c46f2975246407
Zexa <zexa@cock.li>
2021-01-31 05:02:37 +0200

    Add reports generation

--- src/Generator/CsvReportGenerator.php
+++ src/Generator/CsvReportGenerator.php
@@ -5,12 +5,9 @@ declare(strict_types=1);
 namespace App\Generator;
 
 use App\Entity\File;
-use App\Entity\PublicTask;
 use App\Entity\ReportParameters;
 use App\Serializer\PublicTaskSerializer;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\Serializer\Exception\ExceptionInterface;
-use Symfony\Component\Serializer\Normalizer\GetSetMethodNormalizer;
 
 class CsvReportGenerator implements ReportGeneratorInterface
 {
